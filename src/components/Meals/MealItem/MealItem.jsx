74e6e0058dfa347a89efb8ac773441f2d3ca6b48
Jaime Orlando <jaimeorlando.lpz@outlook.com>
2022-07-20 15:23:18 -0500

    (I) MealItemForm: Adding form to enter desired Meal qty.

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,4 +1,6 @@
 import classes from "./MealItem.module.css"
+import MealItemForm from "./MealItemForm";
+
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
     return (
@@ -15,7 +17,7 @@ const MealItem = (props) => {
                 </div>
             </div>
             <div>
-                
+                <MealItemForm id={props.id}/>
             </div>
         </li>
     );

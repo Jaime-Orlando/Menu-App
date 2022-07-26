eb6b8f2ec2f2f001a9b2c4405f298a571e3fceb2
Jaime Orlando <jaimeorlando.lpz@outlook.com>
2022-07-26 16:24:04 -0500

    (I) Cart: Adding and removing items from the Cart.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,11 +10,18 @@ import classes from "./Cart.module.css"
 const Cart = (props) =>
 {
     const cartCtx = useContext(CartContext);
-    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    console.log(cartCtx.totalAmount.toFixed(2));
+    const totalAmount = `$${cartCtx.totalAmount}`;
     const hasItems = cartCtx.items.length > 0;
 
-    const cartItemRemoveHandler = () => {};
-    const cartItemAddHandler = () => {};
+    const cartItemRemoveHandler = (id) => 
+    {
+        cartCtx.removeItem(id);
+    };
+    const cartItemAddHandler = (item) => 
+    {
+        cartCtx.addItem({...item, amount: 1});
+    };
 
     const cartItems = 
     <ul className={classes["cart-items"]}>
@@ -28,8 +35,8 @@ const Cart = (props) =>
         <Modal onClose = {props.onClose}>
             {cartItems}
             <div className={classes.total}>
+                <span>Total Amount</span>
                 <span>{totalAmount}</span>
-                <span>90.88</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>

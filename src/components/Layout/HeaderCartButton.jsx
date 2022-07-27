27a48a27d36e1a7b5aa039bac79836e146cb3844
Jaime Orlando <jaimeorlando.lpz@outlook.com>
2022-07-27 14:24:08 -0500

    (I) HeaderCartButton: Adding bump animation to the cart button every time an item is added or removed.

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
@@ -6,12 +6,43 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = props => 
 {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item)=>{
+    const { items } = cartCtx;
+    const numberOfCartItems = items.reduce((currentNumber, item)=>{
         return currentNumber + item.amount;
     }, 0);
+
+    
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+   
+    useEffect(()=>
+    {
+        if(items.length === 0)
+        {
+            return;
+        }
+
+        // Play Bump animation
+        setBtnIsHighlighted(true);
+
+        // Remove bump css class after playing the animation (which lasts 300 ms.)
+        const timer = setTimeout(()=>
+        {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        // Good Practice: Effect clear function
+        return () => 
+        {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
+
     return (
-    <button className={classes.button} onClick = {props.onClick}>
+    <button className={btnClasses} onClick = {props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
